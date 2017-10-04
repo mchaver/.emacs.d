3c70df64f0da62db351c8ee3f5ab4df7040c954b
James Haver <mchaver@gmail.com>
2017-10-04 10:36:54 +0800

    Add transpose lines functions, assign to ctrl shift up and down

--- init.el
+++ init.el
@@ -190,6 +190,24 @@
 
 (global-set-key [(shift return)] 'smart-open-line)
 
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key [(control shift up)]  'move-line-up)
+(global-set-key [(control shift down)]  'move-line-down)
+
 ;; deft settings
 
 (setq deft-directory "~/deft")
