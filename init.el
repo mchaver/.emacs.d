1a0a5b69fd718ff1d2f749bc300a613ed8897c82
James Haver <mchaver@gmail.com>
2019-04-08 12:41:38 +0200

    Add toggle-camelcase-underscores function

--- init.el
+++ init.el
@@ -27,10 +27,11 @@
 
 ;; load init files
 (safe-add-to-load-path "~/.emacs.d/init")
-(safe-add-to-load-path "~/.emacs.d/init/reason-mode")
+;; (safe-add-to-load-path "~/.emacs.d/init/reason-mode")
 
 ;; (add-to-list 'load-path "~/.opam/4.02.3/share/emacs/site-lisp")
 (safe-load "/home/james/.opam/4.02.3/share/emacs/site-lisp/tuareg-site-file.el")
+;; (safe-load "~/.opam/4.02.3/share/emacs/site-lisp/tuareg-site-file.el")
 
 (require 'cl)
 
@@ -43,17 +44,23 @@
 (setenv "PATH" (concat "/usr/local/bin:/opt/local/bin:/usr/bin:/bin:/usr/local/share/npm/bin:Users/mchaver/.cargo/bin:" (getenv "PATH")))
 
 (defvar mchaver/packages '(ac-slime
+                           ag
 			   auto-complete
 			   autopair
 			   deft
+                           exec-path-from-shell
 			   flycheck
 			   ghc
+                           groovy-mode
+                           js2-mode
+                           json-mode
 			   ido
 			   magit
 			   markdown-mode
 			   marmalade
 			   neotree
 			   org
+                           protobuf-mode
                            rjsx-mode
 			   rust-mode
 			   smex
@@ -333,3 +340,83 @@
   (setq web-mode-css-indent-offset 2)  
 )
 (add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; gradle
+(add-to-list 'auto-mode-alist '("\\.gradle$" . groovy-mode))
+
+
+;; ;; ;; eslint
+;; ;; http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
+
+;; ;; use web-mode for .jsx files
+;; (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; ;; http://www.flycheck.org/manual/latest/index.html
+;; (require 'flycheck)
+
+;; ;; turn on flychecking globally
+;; (add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; ;; disable jshint since we prefer eslint checking
+;; (setq-default flycheck-disabled-checkers
+;;   (append flycheck-disabled-checkers
+;;     '(javascript-jshint)))
+
+;; ;; use eslint with web-mode for jsx files
+;; (flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; ;; customize flycheck temp file prefix
+;; (setq-default flycheck-temp-prefix ".flycheck")
+
+;; ;; disable json-jsonlist checking for json files
+;; (setq-default flycheck-disabled-checkers
+;;   (append flycheck-disabled-checkers
+;;     '(json-jsonlist)))
+
+;; ;; https://github.com/purcell/exec-path-from-shell
+;; ;; only need exec-path-from-shell on OSX
+;; ;; this hopefully sets up path and other vars better
+;; (when (memq window-system '(mac ns))
+;;   (exec-path-from-shell-initialize))
+
+;; ;; use local eslint from node_modules before global
+;; ;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+;; (defun my/use-eslint-from-node-modules ()
+;;   (let* ((root (locate-dominating-file
+;;                 (or (buffer-file-name) default-directory)
+;;                 "node_modules"))
+;;          (eslint (and root
+;;                       (expand-file-name "node_modules/eslint/bin/eslint.js"
+;;                                         root))))
+;;     (when (and eslint (file-executable-p eslint))
+;;       (setq-local flycheck-javascript-eslint-executable eslint))))
+;; (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+;; ;; for better jsx syntax-highlighting in web-mode
+;; ;; - courtesy of Patrick @halbtuerke
+;; (defadvice web-mode-highlight-part (around tweak-jsx activate)
+;;   (if (equal web-mode-content-type "jsx")
+;;     (let ((web-mode-enable-part-face nil))
+;;       ad-do-it)
+;;     ad-do-it))
+
+(global-set-key (kbd "M-\"") 'insert-pair)
+
+;;
+
+(defun toggle-camelcase-underscores ()
+  "Toggle between camelcase and underscore notation for the symbol at point."
+  (interactive)
+  (save-excursion
+    (let* ((bounds (bounds-of-thing-at-point 'symbol))
+           (start (car bounds))
+           (end (cdr bounds))
+           (currently-using-underscores-p (progn (goto-char start)
+                                                 (re-search-forward "_" end t))))
+      (if currently-using-underscores-p
+          (progn
+            (upcase-initials-region start end)
+            (replace-string "_" "" nil start end)
+            (downcase-region start (1+ start)))
+        (replace-regexp "\\([A-Z]\\)" "_\\1" nil (1+ start) end)
+        (downcase-region start (cdr (bounds-of-thing-at-point 'symbol)))))))
